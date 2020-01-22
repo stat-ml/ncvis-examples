c0004bbf3906d51dc3a79fca4ca32d583145b371
Aleksandr Artemenkov <alartum@gmail.com>
2020-01-22 03:25:44 +0300

    Increased images' quality

--- img/gen_png.sh
+++ img/gen_png.sh
@@ -1,3 +1,3 @@
 for i in *.pdf; do
-convert "$i" -resize 400 -background white -alpha background -alpha off -compress zip +adjoin "${i%%.pdf}.png";
+convert "$i" -resize 800 -background white -alpha background -alpha off -compress zip +adjoin "${i%%.pdf}.png";
 done
